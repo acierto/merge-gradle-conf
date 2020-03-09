a536967e84e3d865f0fb1f00cefa57f59ba86809
acierto <bogdannechiporenko@gmail.com>
2020-03-09 08:15:51 +0100

    Fixed found issues in bin/merge.js and incorrectly placed yargs dependency

--- bin/merge.js
+++ bin/merge.js
@@ -1,4 +1,6 @@
 #! /usr/bin/env node
+const path = require('path');
+const projectDir = path.resolve(__dirname, '..');
+
 const shell = require("shelljs");
-console.log('merging...'); // eslint-disable-line
-shell.exec("node ./dist/index.js");
+shell.exec(`node ${projectDir}/dist/index.js`);
