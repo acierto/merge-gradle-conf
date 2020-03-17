1151813d7b489b3858ffbc13e16b0d0730e8bd5a
acierto <bogdannechiporenko@gmail.com>
2020-03-17 08:00:08 +0100

    Updated libs and fixed jest tests

--- test/unit/parse-spec.js
+++ test/unit/parse-spec.js
@@ -1,9 +1,11 @@
+import fs from 'fs';
 import {parseFile} from '../../app/parse';
 import paths from '../../gulp/utils/paths';
 
 describe('Parse', () => {
     it('should parse a file and return back json object', () => {
-        expect(parseFile(`${paths.testDir}/unit/dependencies1.conf`)).toEqual({
+        expect(parseFile(fs.readFileSync(`${paths.testDir}/unit/dependencies1.conf`, 'utf-8'))).toEqual({
+            abcPlugin: '1.1.1',
             myBestPluginVersion: '6.0.0',
             greenFieldPluginVersion: '0.0.1-alpha.108',
             somePluginVersion: '0.0.1+2013.03.13-14.47.00'
